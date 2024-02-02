0f0e9c69ff0bdc2cadb6f938add1418f3bb65461
Adarsh <singhadarsh100@gmail.com>
2024-02-02 18:25:53 +0530

    cors

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 require("dotenv").config();
-require('express-async-errors');
+require("express-async-errors");
 
 const connectDB = require("./db/connect");
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 const mainRouter = require("./routes/user");
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors({ origin: "https://scrapper-project-nine.vercel.app" }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
 
 app.use("/api/v1", mainRouter);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-
-    try {        
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}`);
-        })
-
-    } catch (error) {
-       console.log(error); 
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
-
